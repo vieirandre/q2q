e4c14ca25233b0a6f29642d87cf6794277e6af16
vieirandre <andre.vieira@outlook.com>
2025-02-04 01:16:16 -0300

    Deduplicate src queue msgs on id

--- q2q/src/q2q.cs
+++ q2q/src/q2q.cs
@@ -6,10 +6,12 @@ namespace q2q;
 public class q2q : Iq2q
 {
     private readonly IAmazonSQS _sqsClient;
+    private readonly HashSet<string> _sourceQueueMessageIds;
 
     public q2q()
     {
         _sqsClient = new AmazonSQSClient();
+        _sourceQueueMessageIds = [];
     }
 
     public async Task ForwardMessages(string sourceQueueUrl, string destinationQueueUrl, CancellationToken cancellationToken)
@@ -26,13 +28,14 @@ public class q2q : Iq2q
 
             var receiveResponse = await _sqsClient.ReceiveMessageAsync(receiveRequest, cancellationToken);
 
-            if (receiveResponse.Messages.Count == 0)
-                continue;
+            receiveResponse.Messages.RemoveAll(msg => _sourceQueueMessageIds.Contains(msg.MessageId));
 
             foreach (var message in receiveResponse.Messages)
             {
                 try
                 {
+                    _sourceQueueMessageIds.Add(message.MessageId);
+
                     var sendRequest = new SendMessageRequest
                     {
                         QueueUrl = destinationQueueUrl,

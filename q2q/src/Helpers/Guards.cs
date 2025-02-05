bb170442bfea1c62a14a85fb61f9bfc81da228cc
vieirandre <andre.vieira@outlook.com>
2025-02-05 00:05:33 -0300

    Add static class for guard methods

---
+++ q2q/src/Helpers/Guards.cs
@@ -0,0 +1,12 @@
+﻿namespace q2q.Helpers;
+
+public static class Guards
+{
+    public static int EnsureInRange(int value, string parameterName, int min, int max)
+    {
+        if (value < min || value > max)
+            throw new ArgumentOutOfRangeException(parameterName, value, $"{parameterName} must be between {min} and {max}");
+
+        return value;
+    }
+}

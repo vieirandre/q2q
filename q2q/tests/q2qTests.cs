17ecefefa2ebd2202e8ea4ca36251705c33ad849
vieirandre <andre.vieira@outlook.com>
2025-02-09 06:06:34 -0300

    Test deduplication

--- q2q/tests/q2qTests.cs
+++ q2q/tests/q2qTests.cs
@@ -74,7 +74,75 @@ public class q2qTests
 
         var q2q = new q2q(_sqsClientMock.Object, _options.Object);
 
-        using var cts = new CancellationTokenSource(200);
+        using var cts = new CancellationTokenSource(100);
+
+        // act
+
+        var forwardTask = q2q.ForwardMessages(sourceQueueUrl, destinationQueueUrl, cts.Token);
+        await Assert.ThrowsAsync<TaskCanceledException>(async () => await forwardTask);
+
+        // assert
+
+        _sqsClientMock.Verify(sqs => sqs.SendMessageBatchAsync(
+                It.Is<SendMessageBatchRequest>(req =>
+                    req.QueueUrl == destinationQueueUrl &&
+                    req.Entries.Any(e => e.Id == testMessage.MessageId)),
+                It.IsAny<CancellationToken>()),
+            Times.Once);
+    }
+
+    [Fact]
+    public async Task ForwardMessages_GivenDuplicateMessages_ShouldOnlyForwardOnce()
+    {
+        // arrange
+
+        string sourceQueueUrl = "https://sqs-1";
+        string destinationQueueUrl = "https://sqs-2";
+
+        var testMessage = new Message
+        {
+            MessageId = "msg-1",
+            Body = "Test msg",
+            MessageAttributes = []
+        };
+
+        _sqsClientMock.SetupSequence(sqs => sqs.ReceiveMessageAsync(
+                It.IsAny<ReceiveMessageRequest>(),
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new ReceiveMessageResponse
+            {
+                Messages = [testMessage]
+            })
+            .ReturnsAsync(new ReceiveMessageResponse
+            {
+                Messages = [testMessage]
+            })
+            .ReturnsAsync(new ReceiveMessageResponse
+            {
+                Messages = []
+            });
+
+        _sqsClientMock.Setup(sqs => sqs.SendMessageBatchAsync(
+                It.IsAny<SendMessageBatchRequest>(),
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync((SendMessageBatchRequest request, CancellationToken token) =>
+            {
+                var response = new SendMessageBatchResponse();
+
+                foreach (var entry in request.Entries)
+                {
+                    response.Successful.Add(new SendMessageBatchResultEntry
+                    {
+                        Id = entry.Id
+                    });
+                }
+
+                return response;
+            });
+
+        var q2q = new q2q(_sqsClientMock.Object, _options.Object);
+
+        using var cts = new CancellationTokenSource(100);
 
         // act
 
